28ecc71cff8d93211205eb42cfb31a20622e2092
Dictrct <uppcl.purvanchal@gmail.com>
2026-09-15 22:20:58 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,15 +1,17 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-analytics.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBhH4Um5AUqY9-b8Ls966t7GGbhHM3zemo",
-  authDomain: "shibli-collage.firebaseapp.com",
-  projectId: "shibli-collage",
-  storageBucket: "shibli-collage.firebasestorage.app",
-  messagingSenderId: "381888344569",
-  appId: "1:381888344569:web:e33390484db5e3e8d68d28",
-  measurementId: "G-Q56TY4QJ76"
+    apiKey: "AIzaSyBhH4Um5AUqY9-b8Ls966t7GGbhHM3zemo",
+    authDomain: "shibli-collage.firebaseapp.com",
+    projectId: "shibli-collage",
+    storageBucket: "shibli-collage.firebasestorage.app",
+    messagingSenderId: "381888344569",
+    appId: "1:381888344569:web:e33390484db5e3e8d68d28",
+    measurementId: "G-Q56TY4QJ76"
 };
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export const db = getFirestore(app);
